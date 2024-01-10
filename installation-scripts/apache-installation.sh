4d2862ab668e4b291efd9a8bd67825523887960f
Nagaraju Kusa <nagaraju.kusa@thoughtworks.com>
2024-01-10 21:54:23 +0530

    Added script to install apache server

---
+++ installation-scripts/apache-installation.sh
@@ -0,0 +1,10 @@
+echo "Installing Apache server on EC2 instance"
+
+echo "updating the entire system dependencies & libraries"
+sudo apt update
+
+echo "using the apt package manager and install apache server"
+sudo apt install apache2
+
+echo "displays the status of apache server"
+sudo systemctl status apache2
