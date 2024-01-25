5cd99bcda3972e6230d0db2b743844a53251ebae
infinitymasters <devops24online@gmail.com>
2024-01-25 22:13:37 +0530

    Added jenkins installation script

---
+++ installation-scripts/jenkins-installation.sh
@@ -0,0 +1,15 @@
+# Downloads the jenkins key from website
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Updates system binaries / dependencies
+sudo apt-get update
+
+# Installs jenkins on instance / machine
+sudo apt-get install jenkins
+
+# Displays status of jenkins -> running / stopped
+sudo systemctl status jenkins
