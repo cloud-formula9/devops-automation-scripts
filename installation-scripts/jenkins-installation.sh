a9612fefde4256590aaa5dff5995bb722fb0a5e1
infinitymasters <devops24online@gmail.com>
2024-01-26 22:12:50 +0530

    Added script for jenkins installation

--- installation-scripts/jenkins-installation.sh
+++ installation-scripts/jenkins-installation.sh
@@ -1,15 +1,26 @@
-# Downloads the jenkins key from website
+# Downloads a key to validate and verify jenkins installation
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-# Updates system binaries / dependencies
+# Updates system binaries & dependencies
+echo "Updating system libraries"
 sudo apt-get update
+echo "===================================="
 
-# Installs jenkins on instance / machine
-sudo apt-get install jenkins -y
+# Installs Jenkins
+echo "Installing Jenkins CI/CD"
+sudo apt-get install jenkins
+echo "===================================="
+
+# starts jenkins server
+echo "Trying to start jenkins server"
+sudo systemctl start jenkins
+echo "===================================="
 
 # Displays status of jenkins -> running / stopped
+echo "Here is the status of jenkins"
 sudo systemctl status jenkins
+echo "===================================="
