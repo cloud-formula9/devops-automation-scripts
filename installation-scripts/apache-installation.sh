708601723c975d147b0b5e7faddf004be9ec02d6
Infinity Masters Online <134763582+infinitymasters@users.noreply.github.com>
2024-01-31 23:02:19 +0530

    Update apache-installation.sh

--- installation-scripts/apache-installation.sh
+++ installation-scripts/apache-installation.sh
@@ -4,7 +4,7 @@ echo "updating the entire system dependencies & libraries"
 sudo apt update
 
 echo "using the apt package manager and install apache server"
-sudo apt install apache2
+sudo apt install apache2 -y
 
 echo "displays the status of apache server"
 sudo systemctl status apache2
